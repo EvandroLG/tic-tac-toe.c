dad3ffc6c1a915e749138ad89bb527774f85f13a
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-08-30 22:11:41 +0100

    add logic to handle computer moves

--- main.c
+++ main.c
@@ -34,6 +34,22 @@ void makeMove(int *board, int square, int side) {
     board[square] = side;
 }
 
+int getComputerMove(int *board) {
+    int freeSpaces = 0;
+    int randomMove = 0;
+    int availableMoves[9];
+
+    for (int i = 0; i < 9; i++) {
+        if (board[CONVERT_TO_25[i]] == EMPTY) {
+            availableMoves[freeSpaces++] = CONVERT_TO_25[i];
+        }
+    }
+
+    randomMove = (rand() % freeSpaces);
+
+    return availableMoves[randomMove];
+}
+
 int getHumanMove(int *board) {
     char userInput[4];
     int moveOk = 0;
@@ -94,6 +110,7 @@ void printBoard(int *board) {
 
 void runGame() {
     int gameOver = 0;
+    int lastMoveMade = 0;
     int side = NOUGHTS;
     int board[25];
 
@@ -102,10 +119,22 @@ void runGame() {
 
     while (!gameOver) {
         if (side == NOUGHTS) {
-            getHumanMove(&board[0]);
+            lastMoveMade = getHumanMove(&board[0]);
+            makeMove(&board[0], lastMoveMade, side);
+            side = CROSSES;
         } else {
+            lastMoveMade = getComputerMove(&board[0]);
+            makeMove(&board[0], lastMoveMade, side);
+            side = NOUGHTS;
+
             printBoard(&board[0]);
         }
+    }
+
+    if (!hasEmpty(board)) {
+        printf("Game over!\n");
+        gameOver = 1;
+        printf("It's a draw\n");
     }
 }
 
